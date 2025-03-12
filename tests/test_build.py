dff8e55c6a9be9fcfc56dd08f1754dd41abfa2af
Mihai Capotă <mihai@mihaic.ro>
2025-03-12 08:57:41 -0700

    Fix float16 dynamic build (#3)

--- tests/test_build.py
+++ tests/test_build.py
@@ -13,24 +13,36 @@ def test_build_static(svs_type, tmp_vecs):
         svs_type.startswith("float")
         and svs_type != SUFFIX_TO_SVS_TYPE[tmp_vecs.suffix]
     ):
-        pytest.skip("Not supported")
-    svsbench.build.build_static(
-        vecs_path=tmp_vecs,
-        svs_type=svs_type,
-        distance=svs.DistanceType.L2,
-    )
+        with pytest.raises(ValueError, match="Expected svs_type"):
+            svsbench.build.build_static(
+                vecs_path=tmp_vecs,
+                svs_type=svs_type,
+                distance=svs.DistanceType.L2,
+            )
+    else:
+        svsbench.build.build_static(
+            vecs_path=tmp_vecs,
+            svs_type=svs_type,
+            distance=svs.DistanceType.L2,
+        )
 
 @pytest.mark.parametrize("svs_type", svsbench.consts.SVS_TYPES)
 def test_build_dynamic(svs_type, tmp_vecs):
+    if SUFFIX_TO_SVS_TYPE[tmp_vecs.suffix] == "float16":
+        pytest.xfail("https://github.com/intel/ScalableVectorSearch/issues/93")
     if (
         svs_type.startswith("float")
         and svs_type != SUFFIX_TO_SVS_TYPE[tmp_vecs.suffix]
     ):
-        pytest.skip("Not supported")
-    if SUFFIX_TO_SVS_TYPE[tmp_vecs.suffix] == "float16":
-        pytest.xfail("https://github.com/intel/ScalableVectorSearch/issues/93")
+        with pytest.raises(ValueError, match="Expected svs_type"):
+            svsbench.build.build_dynamic(
+                vecs_path=tmp_vecs,
+                svs_type=svs_type,
+                distance=svs.DistanceType.L2,
+            )
     svsbench.build.build_dynamic(
         vecs_path=tmp_vecs,
         svs_type=svs_type,
         distance=svs.DistanceType.L2,
+        convert_vecs=True,
     )
